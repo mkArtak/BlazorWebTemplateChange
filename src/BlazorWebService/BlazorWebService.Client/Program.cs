13268322dcf8f0dad44cf874cb8a61dc93e49b14
mkArtak <artak.services+github@outlook.com>
2024-05-04 17:54:54 -0700

    Move Weather page to the client and call server API for the data

--- src/BlazorWebService/BlazorWebService.Client/Program.cs
+++ src/BlazorWebService/BlazorWebService.Client/Program.cs
@@ -2,4 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+// Register an HttpClient service to use for calling APIs on the server
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 await builder.Build().RunAsync();
---
+++ src/BlazorWebService/BlazorWebService/Services/WeatherForecastService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebService.Client;
+
+namespace BlazorWebService.Services;
+
+internal class WeatherForecastService
+{
+    public async Task<WeatherForecast[]?> GetWeatherForecastAsync()
+    {
+        // Simulate asynchronous loading
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToArray();
+    }
+}
